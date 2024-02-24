9ed6c4b7e2fae3b31ff20360a8f5b3b1d27bcb24
Justin Kruskie <kruskiej@baisd.net>
2024-02-24 17:22:07 +0000

    Create assignment

---
+++ AstronautExam.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AstronautExam {
+
+    public static void main(String[] args) {
+        // Variables. You do not need more than these.
+        int tasks;          // Number of tasks completed
+        int percentage;     // Percentage
+
+        // Create a Scanner object for keyboard input.
+
+        // Get the number of tasks completed.
+
+        // Close the keyboard.
+
+        // Determine the percentage.
+
+        // Display the percentage.
+
+        // Close the application.
+        
+    }
+
+}
+
